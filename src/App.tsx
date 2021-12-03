8b7a89e69be26cbc4aad00f0fdd2b17ff036cc33
ahmadysalah <a.ahmad.salah@gmail.com>
2021-12-03 23:12:48 +0200

    install formik

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,13 @@
-import React, { useEffect } from 'react';
-import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { toDoListConstants } from './redux/constants';
-import asyncStore from './redux/thunk';
+import ReduxComponent from './pages/ReduxEx';
+import Form from './pages/Form';
 
-interface obj {
-  [key: string]: any
-}
+
 function App() {
-  const [toDoListInput, setToDoList] = React.useState('');
-  const dispatch = useDispatch();
-  const { toDoList: { toDos } }: obj = useSelector(state => state);
-
-  useEffect(() => {
-    dispatch(asyncStore.getState());
-  }, [dispatch]);
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    dispatch(asyncStore?.ADD_TODO(toDoListInput));
-  }
-
   return (
-    <div className="App">
-      <h1>To Do List</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="input-container">
-          <input type="text" value={toDoListInput} onChange={(e) => setToDoList(e.target.value)} />
-          <button type='submit'>Add</button>
-        </div>
-      </form>
-      <ul>
-        {[...toDos]?.reverse().map((item: any, index: number) => (
-          <li key={index}
-            className='input-container'
-          >
-            <p
-              onClick={() => dispatch({ type: toDoListConstants.TOGGLE_TODO, payload: index })}
-              style={{ textDecoration: item.completed ? 'line-through' : 'none' }}
-            > {item.title} </p>
-            <button onClick={() => dispatch({ type: toDoListConstants.DELETE_TODO, payload: item.id })}>Remove</button>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <ReduxComponent />
+      <Form />
+    </>
   );
 }
 export default App;
